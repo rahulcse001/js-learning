874d79d15167c6e92ddfc624d775acb0234f5c50
RAHUL KUMAR <85482023+rahulcse001@users.noreply.github.com>
2026-01-31 17:27:47 +0000

    variable

---
+++ 01_Basic/variable.js
@@ -0,0 +1,12 @@
+const accountId = 12345;
+
+let accountName = "rahul";
+
+var age= 25
+
+accountName = 'testing123';
+
+console.log(" demo testing console");
+
+console.table([accountId,accountName,age])
+
